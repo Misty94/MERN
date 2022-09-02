78e0e83655e93aaed80a86678a14644ceb72ad3e
Misty94 <mms.strickland@gmail.com>
2022-09-02 13:49:45 -0400

    Validations Added

--- Week1/react/strickland-misty-forms/src/components/HookForm.jsx
+++ Week1/react/strickland-misty-forms/src/components/HookForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Display from './Display';
 
-const HookForm = (props) => {
+const HookForm = () => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
@@ -20,22 +20,27 @@ const HookForm = (props) => {
                 <div className="mb-3">
                     <label className="me-5">First Name: </label>
                     <input type="text" onChange={ (e) => setFirstName(e.target.value)} value={ firstName }/>
+                    { firstName.length < 2 && firstName.length > 0 ? <p className="text-danger">First Name must be at least 2 characters long!</p> : '' }
                 </div>
                 <div className="mb-3">
                     <label className="me-5">Last Name: </label>
                     <input type="text" onChange={ (e) => setLastName(e.target.value)} value={ lastName }/>
+                    { lastName.length < 2 && lastName.length > 0 ? <p className="text-danger">Last Name must be at least 2 characters long!</p> : '' }
                 </div>
                 <div className="mb-3">
                     <label className="me-5">Email: </label>
                     <input type="email" onChange={ (e) => setEmail(e.target.value)} value={ email }/>
+                    { email.length < 5 && email.length > 0 ? <p className="text-danger">Email must be at least 5 characters long!</p> : '' }
                 </div>
                 <div className="mb-3">
                     <label className="me-5">Password: </label>
                     <input type="password" onChange={ (e) => setPassword(e.target.value)} value={ password }/>
+                    { password.length < 8 && password.length > 0 ? <p className="text-danger">Your password must be at least 8 characters long!</p> : '' }
                 </div>
                 <div className="mb-3">
                     <label className="me-2">Confirm Password: </label>
                     <input type="password" onChange={ (e) => setConfirm(e.target.value)} value={ confirm }/>
+                    { confirm != password && confirm.length > 0 ? <p className="text-danger">Your passwords must match!</p> : '' }
                 </div>
                 {/* <input type="submit" className="my-2" value="Create User" /> */}
             </form>
